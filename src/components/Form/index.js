068cf0d81bc6558d98de520d78c338515284a093
andrecosta02 <131416835+andrecosta02@users.noreply.github.com>
2023-09-15 19:51:03 -0300

    slá

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -11,15 +11,69 @@ const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
   const [isExpense, setExpense] = useState(false);
   const [date, setDate] = useState(null);
 
+  // const dateFormatAux = (date) => {
+  //   var d = new Date(date),
+  //     month = '' + (d.getMonth() + 1),
+  //     day = '' + d.getDate(),
+  //     year = d.getFullYear();
+
+  //   if (month.length < 2)
+  //     month = '0' + month;
+  //   if (day.length < 2) 
+  //     day = '0' + day;
+    
+  //   return [year, month, day].join('-');
+  // }
+
+  // const dateFormat = (date) => {
+
+  //   console.log(new Date(date));
+
+  //   let formatYearMonthDay = dateFormatAux(date);
+  //   //console.log(formatYearMonthDay);
+
+  //   let formatISO8601 = new Date(date).toISOString();
+  //   //console.log(formatISO8601);
+
+  //   return [formatYearMonthDay, formatISO8601];
+  // }
+
+  // const dateFormat = (date) => {
+  //   var d = new Date(date),
+  //     month = '' + (d.getMonth() + 1),
+  //     day = '' + d.getDate(),
+  //     year = d.getFullYear();
+
+  //   if (month.length < 2)
+  //     month = '0' + month;
+  //   if (day.length < 2) 
+  //     day = '0' + day;
+    
+  //   return [year, month, day].join('-');
+
+  // }
+
+  // const dateFormat = (date) => {
+
+  //   let formatYearMonthDay = dateFormatAux(date)
+  //   return [formatYearMonthDay];
+  // }
+
   const generateID = () => Math.round(Math.random() * 1000);
 
   const handleSave = () => {
+    // console.log(dateFormat)
+    // console.log(dateFormatAux)
     if (!desc || !amount) {
       alert("Informe a descrição e o valor!");
       return;
     } else if (amount < 1) {
       alert("O valor tem que ser positivo!");
       return;
+    }
+
+    if (!desc || !amount) {
+      
     }
 
     const transaction = {
@@ -32,6 +86,7 @@ const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
 
     handleAdd(transaction);
 
+    setDate("");
     setDesc("");
     setAmount("");
   };
@@ -42,18 +97,34 @@ const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
 
         <C.InputContent>
           <C.Label>Data</C.Label>
-          {/* <DatePicker  /> */}
-          <DatePicker value={date} selected={date} onChange={(date) => setDate(date)} dateFormat="dd/MM/yyyy" placeholderText="Ex: 12/09/2023"/>
+          <DatePicker 
+            value={date} 
+            selected={date} 
+            onChange={(date) => setDate(date)} 
+            dateFormat="dd/MM/yyyy" 
+            placeholderText="Ex: 12/09/2023"
+            // className="data"
+            // id="data"
+          />
         </C.InputContent>
 
         <C.InputContent>
           <C.Label>Descrição</C.Label>
-          <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} placeholder="Ex: Recebimento do salário"/>
+          <C.Input 
+            value={desc} 
+            onChange={(e) => setDesc(e.target.value)} 
+            placeholder="Ex: Recebimento do salário"
+          />
         </C.InputContent>
 
         <C.InputContent>
           <C.Label>Valor</C.Label>
-          <C.Input value={amount} type="number" onChange={(e) => setAmount(e.target.value)}  placeholder="Ex: 1320"/>
+          <C.Input 
+            value={amount} 
+            type="number" 
+            onChange={(e) => setAmount(e.target.value)}  
+            placeholder="Ex: 1320"
+          />
         </C.InputContent>
         
         <C.RadioGroup>
